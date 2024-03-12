9b38e76ddaaa3f8065d029786a9fde8aa74cfe6e
Santiago Bedoya <santiagobedoya@live.com>
2024-03-12 00:17:34 -0500

    Create CRUD for Categories

---
+++ Orders/Orders.Frontend/Pages/Categories/CategoryCreate.razor.cs
@@ -0,0 +1,42 @@
+﻿using CurrieTechnologies.Razor.SweetAlert2;
+using Microsoft.AspNetCore.Components;
+using Orders.Frontend.Pages.Countries;
+using Orders.Frontend.Repositories;
+using Orders.Shared.Entities;
+
+namespace Orders.Frontend.Pages.Categories
+{
+    public partial class CategoryCreate
+    {
+        private CategoryForm? categoryForm;
+        private Category category = new();
+        [Inject] public IRepository repository { get; set; } = null!;
+        [Inject] private SweetAlertService sweetAlertService { get; set; } = null!;
+        [Inject] private NavigationManager navigationManager { get; set; } = null!;
+
+        private async Task CreateAsync()
+        {
+            var responseHttp = await repository.PostAsync("api/categories", category);
+            if (responseHttp.Error)
+            {
+                var message = await responseHttp.GetErrorMessageAsync();
+                await sweetAlertService.FireAsync("Error", message, SweetAlertIcon.Error);
+                return;
+            }
+            var toast = sweetAlertService.Mixin(new SweetAlertOptions
+            {
+                Toast = true,
+                Position = SweetAlertPosition.BottomEnd,
+                ShowConfirmButton = true,
+                Timer = 3000
+            });
+            Return();
+        }
+
+        private void Return()
+        {
+            categoryForm!.FormPostedSuccessfully = true;
+            navigationManager.NavigateTo("/categories");
+        }
+    }
+}
