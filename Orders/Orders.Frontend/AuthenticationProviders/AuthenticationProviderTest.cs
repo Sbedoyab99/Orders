11d70686a97d872466898016ed7d076845219b1e
Santiago Bedoya <santiagobedoya@live.com>
2024-04-15 19:51:41 -0500

    Add user and admin authentication in front

---
+++ Orders/Orders.Frontend/AuthenticationProviders/AuthenticationProviderTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace Orders.Frontend.AuthenticationProviders
+{
+    public class AuthenticationProviderTest : AuthenticationStateProvider
+    {
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            await Task.Delay(3000);
+            var anonimous = new ClaimsIdentity();
+            var user = new ClaimsIdentity(authenticationType: "test");
+            var admin = new ClaimsIdentity(new List<Claim>
+            {
+                new Claim("FirstName", "Santiago"),
+                new Claim("LastName", "Bedoya"),
+                new Claim(ClaimTypes.Name, "bedoya@yopmail.com"),
+                new Claim(ClaimTypes.Role, "Admin")
+            }, authenticationType: "test");
+            return await Task.FromResult(new AuthenticationState(new ClaimsPrincipal(user)));
+        }
+    }
+}
