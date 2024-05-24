61b2de2d77645b6a914694b500b9e3337e07db46
Santiago Bedoya <santiagobedoya@live.com>
2024-05-23 19:14:59 -0500

    Order Index for Admins

--- Orders/Orders.Backend/Controllers/OrdersController.cs
+++ Orders/Orders.Backend/Controllers/OrdersController.cs
@@ -2,6 +2,8 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Orders.Backend.Helpers;
+using Orders.Backend.UnitsOfWork.Implementations;
+using Orders.Backend.UnitsOfWork.Interfaces;
 using Orders.Shared.DTOs;
 using Orders.Shared.Entities;
 
@@ -13,10 +15,12 @@ namespace Orders.Backend.Controllers
     public class OrdersController : ControllerBase
     {
         private readonly IOrdersHelper _ordersHelper;
+        private readonly IOrdersUnitOfWork _ordersUnitOfWork;
 
-        public OrdersController(IOrdersHelper ordersHelper)
+        public OrdersController(IOrdersHelper ordersHelper, IOrdersUnitOfWork ordersUnitOfWork)
         {
             _ordersHelper = ordersHelper;
+            _ordersUnitOfWork = ordersUnitOfWork;
         }
 
         [HttpPost]
@@ -29,6 +33,28 @@ namespace Orders.Backend.Controllers
             }
 
             return BadRequest(response.Message);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAsync([FromQuery] PaginationDTO pagination)
+        {
+            var response = await _ordersUnitOfWork.GetAsync(User.Identity!.Name!, pagination);
+            if (response.WasSuccess)
+            {
+                return Ok(response.Result);
+            }
+            return BadRequest();
+        }
+
+        [HttpGet("totalPages")]
+        public async Task<IActionResult> GetPagesAsync([FromQuery] PaginationDTO pagination)
+        {
+            var action = await _ordersUnitOfWork.GetTotalPagesAsync(User.Identity!.Name!, pagination);
+            if (action.WasSuccess)
+            {
+                return Ok(action.Result);
+            }
+            return BadRequest();
         }
     }
 }
